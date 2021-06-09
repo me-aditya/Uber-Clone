334db537bc20e09cbffecf61bb39ddeb0376b58e
me-aditya <adityarathi1212@gmail.com>
2021-06-09 12:34:18 +0530

    setup provider package

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uber_flutter/dataprovider/app_data.dart';
 import 'package:uber_flutter/screens/loginpage.dart';
 import 'package:uber_flutter/screens/mainpage.dart';
 import 'package:uber_flutter/screens/registration_page.dart';
@@ -24,16 +26,19 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          fontFamily: 'Brand-Regular',
-        ),
-        initialRoute: MainPage.id,
-        routes: {
-          RegistrationPage.id: (context) => RegistrationPage(),
-          MainPage.id: (context) => MainPage(),
-          LoginPage.id: (context) => LoginPage(),
-        });
+    return ChangeNotifierProvider(
+      create: (context) => AppData(),
+      child: MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            fontFamily: 'Brand-Regular',
+          ),
+          initialRoute: MainPage.id,
+          routes: {
+            RegistrationPage.id: (context) => RegistrationPage(),
+            MainPage.id: (context) => MainPage(),
+            LoginPage.id: (context) => LoginPage(),
+          }),
+    );
   }
 }
